1f546e6d832385d9b42e48a0017fd0a436fa8ed1
Sondre Basma <scbasma@dot.cs.ucsb.edu>
2015-06-09 17:45:09 -0700

    further attemps at different sets of messages

--- client/client2.py
+++ client/client2.py
@@ -1,29 +1,36 @@
 import time
 import random
 import urllib
+import json
 
-N = 7893364505739056694752315123831541284296639995794928392341494104718749447912465392151523342505244983911640199474005668676906821577143673549665195711897437
+public_url = "http://localhost:8080/publickey"
+
+response = urllib.urlopen(public_url)
+data = json.loads(response.read())
+
+N = int(data['n'])
+
+print N
 
 def generateZSet():
-	return [random.randint(N**(1/3.0) + 1, N**(1/2.0) - 1) for p in range(200)]
+	z = [random.randint(N**(1/3.0) + 1, N**(1/2.0) - 1) for p in range(100000)]
 
 def generateYSet():
-	return [random.randint(0, N**(1/3.0 - 1)) for p in range(200)]
+	return [random.randint(0, N**(1/3.0)-1) for p in range(100000)]
 
 
 def sendGuess(message):
 	start = time.clock()
-	decrypt_url = "https://polar-coast-7047.herokuapp.com/decrypt?message={0}".format(message)
+	decrypt_url = "http://localhost:8080/decrypt?message={0}".format(message)
 	response = urllib.urlopen(decrypt_url)
 	end = time.clock()
 	return end-start
 
 
 def getDecryptTime(message):
-	print 'getDecryptTime'
 	times = [] 
 	for i in range(1):
-		times.append(sendGuess(message))
+		times.append(sendGuess(bin(message)[2:]))
 	times.sort()
 	return sum(times)/len(times)
 
