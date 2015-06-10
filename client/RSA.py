b77a9442ae2fa1823a3bc94ecc762c0490e23996
Sondre Basma <scbasma@blondie.cs.ucsb.edu>
2015-06-10 15:51:29 -0700

    moved cryptographic specific code into module RSA.py

---
+++ client/RSA.py
@@ -0,0 +1,53 @@
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    gcd, x, y = egcd(a, m)
+    if gcd != 1:
+        return None  # modular inverse does not exist
+    else:
+        return x % m
+
+def generate_r(n):
+    x = 0
+    while(2**x < n):
+        x += 1
+    return 2 ** x
+
+def monPro(a, b, r, n):
+
+    r_inv = modinv(r, n)
+    n_prime = (r * r_inv - 1)/n
+
+    t = a * b
+    m = (t*n_prime) % r
+    u = (t + m*n)/r
+    if (u >= n):
+        return (u-n, True)
+    return (u, False)
+
+
+
+def modExp(M, d, n, r):
+    M_bar = (M*r) % n
+    C_bar = r % n
+    d_guess = d
+    d_guess += '1'
+    reduction = False
+    for ei in d_guess:
+        C_bar, reduction = monPro(C_bar, C_bar, r, n)
+        if(ei == '1'):
+            C_bar, reduction  = monPro(M_bar, C_bar, r, n)
+
+    return reduction
+
+
+def CheckReduction(guess, message, n, r):
+    return modExp(message, guess, n, r)
+
+
+
--- client/client2.py
+++ client/client2.py
@@ -2,6 +2,8 @@ import time
 import random
 import urllib
 import json
+import RSA
+
 
 public_url = "http://localhost:8080/publickey"
 
@@ -12,76 +14,6 @@ N = int(data['n'])
 
 print N
 
-def generateZSet():
-    z = [random.randint(N**(1/3.0) + 1, N**(1/2.0) - 1) for p in range(10000)]
-    m_list = []
-    for message in z:
-        encrypt_url = "http://localhost:8080/encrypt?message={0}".format(message)
-        response = urllib.urlopen(encrypt_url)
-        data = json.loads(response.read())
-        m_list.append(data['signature'])
-
-    return m_list
-def generateYSet():
-    y = [random.randint(0, N**(1/3.0)-1) for p in range(10000)]
-    m_list = []
-
-    for message in y:
-        encrypt_url = "http://localhost:8080/encrypt?message={0}".format(message)
-        response = urllib.urlopen(encrypt_url)
-        data = json.loads(response.read())
-        m_list.append(data['signature'])
-    return m_list
-
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
-
-def modinv(a, m):
-    gcd, x, y = egcd(a, m)
-    if gcd != 1:
-        return None  # modular inverse does not exist
-    else:
-        return x % m
-
-def generate_r(n):
-    x = 0
-    while(2**x < n):
-        x += 1
-    return 2 ** x
-
-def monPro(a, b, r, n):
-
-    r_inv = modinv(r, n)
-    n_prime = (r * r_inv - 1)/n
-
-    t = a * b
-    m = (t*n_prime) % r
-    u = (t + m*n)/r
-    if (u >= n):
-        return (u-n, True)
-    return (u, False)
-
-
-
-def modExp(M, d, n, r):
-    M_bar = (M*r) % n
-    C_bar = r % n
-    d_guess = d
-    d_guess += '1'
-    reduction = False
-    for ei in d_guess:
-        C_bar, reduction = monPro(C_bar, C_bar, r, n)
-        if(ei == '1'):
-            C_bar, reduction  = monPro(M_bar, C_bar, r, n)
-
-    return reduction
-
-def RSACheckReduction(guess, message, n, r):
-    return modExp(message, guess, n, r)
 
 def sendGuess(message):
     decrypt_url = "http://localhost:8080/decrypt?message={0}".format(message)
@@ -103,7 +35,7 @@ def getSampleMessages(size):
 
 
 def doAttack(n):
-    r = generate_r(n)
+    r = RSA.generate_r(n)
     bits_to_solve = 128
     secretKey = '1'
 
@@ -112,7 +44,7 @@ def doAttack(n):
         reduction_list = []
         no_reduction_list = []
         for sample in sample_list:
-            if RSACheckReduction(secretKey, sample, n, r):
+            if RSA.CheckReduction(secretKey, sample, n, r):
                 reduction_list.append(sample)
             else:
                 no_reduction_list.append(sample)
